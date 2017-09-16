8ff030eb3ef2ef9ab27d73a304906e4027f3e78d
Dennis <denn.vet@gmail.com>
2017-09-16 02:00:49 +0200

    added drag and drop to menu items

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -2,22 +2,39 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // Zum Debuggen von Code beim Laden einer Seite in cordova-simulate oder auf Android-Geräten/-Emulatoren: Starten Sie Ihre App, legen Sie Haltepunkte fest, 
 // und führen Sie dann "window.location.reload()" in der JavaScript-Konsole aus.
+move = false;
+startX = 0;
+startY = 0;
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        $(document).mousemove(function (e) {
+            if (move) {
+                $("#movingImage").css({ left: e.pageX, top: e.pageY });
+            }
+        });
+        $(".food-button").on('mousedown', function (event) {
+            move = true;
+            var imageUrl = $(this).children("img").get(0).getAttribute("src");
+            $("#movingImage").css({ left: event.pageX, top: event.pageY });
+            $("#movingImage").attr("src", imageUrl);
+            event.preventDefault();
+            event.stopPropagation();
+        });
+        $("body").on('mouseup', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            move = false;
+            $("#movingImage").attr("src", "");
+            $("#movingImage").css({ left: startX, top: startY });
+        });
     };
 
     function onPause() {
@@ -27,4 +44,4 @@
     function onResume() {
         // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
     };
-} )();
+})();
