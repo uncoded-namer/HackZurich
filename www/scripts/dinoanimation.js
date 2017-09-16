e8131cf87e9841e150b30eed53d6a5000fdb5f45
Josh Tari <josh.tari@dogmacompany.com>
2017-09-16 08:27:16 +0200

    add animations + base logic

--- www/scripts/dinoanimation.js
+++ www/scripts/dinoanimation.js
@@ -1,14 +1,15 @@
 ﻿var canvas;
 var context;
 var images = {};
-var totalResources = 3; // TODO: think
+var totalResources = 15; // TODO: think
 var numResourcesLoaded = 0;
 var fps = 30;
-var charX = 150;
-var charY = 185;
 var fpsInterval = setInterval(updateFPS, 1000);
 var numFramesDrawn = 0;
 var curFPS = 0;
+var mouthState = 0; // 0 = closed, 1 opening, 2 open, 3 closing
+var mouthHalfFrameAmount = 4;
+var mouthHalfFrameCounter = 0;
 
 // TODO: refactor
 
@@ -22,8 +23,7 @@ var recommendedSalt = 2.3;
 var recommendedFiber = 30; // g
 var recommendedWater = 1.5; // L
 var recommendedVegetables = 3; // pieces
-var recommendedFruits = 2; // kilos
-
+var recommendedFruits = 2; // kilo
 
 var energy = recommendedEnergy;
 var protein = recommendedProtein;
@@ -37,6 +37,7 @@ var water = recommendedWater;
 var vegetables = recommendedVegetables;
 var fruits = recommendedFruits;
 
+var fatness = 0; // TODO: remove
 
 function updateFPS() {
 
@@ -46,7 +47,7 @@ function updateFPS() {
 
 function clearCanvas() {
     canvas.width = canvas.width;
-} 
+}
 
 function prepareCanvas(canvasDiv, canvasWidth, canvasHeight) {
     // Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
@@ -56,10 +57,14 @@ function prepareCanvas(canvasDiv, canvasWidth, canvasHeight) {
     canvas.setAttribute('id', 'canvas');
     canvasDiv.appendChild(canvas);
 
+
     if (typeof G_vmlCanvasManager != 'undefined') {
         canvas = G_vmlCanvasManager.initElement(canvas);
     }
     context = canvas.getContext("2d"); // Grab the 2d canvas context
+
+    
+
     // Note: The above code is a workaround for IE 8and lower. Otherwise we could have used:
     //     context = document.getElementById('canvas').getContext("2d");
 
@@ -67,9 +72,21 @@ function prepareCanvas(canvasDiv, canvasWidth, canvasHeight) {
     clearCanvas();
     context.fillText("loading...", 40, 140);
 
-    loadImage("torso");
+    loadImage("body");
+    loadImage("body_fat1");
+    loadImage("body_fat2");
+    loadImage("body_fat3");
+    loadImage("body_fat4");
     loadImage("head");
-    loadImage("eye");
+    loadImage("head_half");
+    loadImage("head_sad1");
+    loadImage("head_sad2");
+    loadImage("head_open");
+    loadImage("left_foot");
+    loadImage("left_hand");
+    loadImage("right_foot");
+    loadImage("right_hand");
+    loadImage("tail");
 }
 
 function loadImage(name) {
@@ -77,8 +94,8 @@ function loadImage(name) {
     images[name] = new Image();
     images[name].onload = function () {
         resourceLoaded();
-    }
-    images[name].src = "images/placeholder/" + name + ".png";
+    };
+    images[name].src = "images/parts/" + name + ".png";
 }
 
 function resourceLoaded() {
@@ -90,61 +107,267 @@ function resourceLoaded() {
     }
 }
 
-function isHealthy() {
-    return false;
+function interpolateValues(startPos, endPos, t) {
+    return startPos + t * (endPos - startPos);
 }
 
-function isRipped() {
-    return false;
+var headBobbingStartPositionY = 0;
+var headBobbingEndPositionY = 5;
+var headBobbingDuration = 180;
+var headBobbingValue = 0;
+var headBobbingOffsetY = 0;
+
+function headBobbingTweening(t) {
+    if (t <= 0.5) {
+        return t * 2;
+    } else {
+        t -= 0.5;
+        t *= 2;
+        return 1 - t;
+    }
 }
 
-function isObese() {
-    return false;
+function updateHeadBobbing() {
+    headBobbingValue = (headBobbingValue + 1) % headBobbingDuration;
+    var t = headBobbingValue / headBobbingDuration;
+    headBobbingOffsetY = interpolateValues(headBobbingStartPositionY, headBobbingEndPositionY, headBobbingTweening(t));
 }
 
-function isStarving() {
-    return false;
+
+var armRotationStart = 0;
+var armRotationEnd = 0.3;
+var armRotationDuration = 120;
+var armaRotationValue = 0;
+var armRotation = armRotationStart;
+
+function armRotationTweening(t) {
+    if (t <= 0.5) {
+        return t * 2;
+    } else {
+        t -= 0.5;
+        t *= 2;
+        return 1 - t;
+    }
+}
+
+function updateArmRotation() {
+    armaRotationValue = (armaRotationValue + 1) % armRotationDuration;
+    var t = armaRotationValue / armRotationDuration;
+    armRotation = interpolateValues(armRotationStart, armRotationEnd, armRotationTweening(t));
+}
+
+var footRotationStart = -0.07;
+var footRotationEnd = 0.07;
+var footRotationDuration = 180;
+var footRotationValue = footRotationDuration / 2;
+var footRotation = 0;
+
+function updateFootRotation() {
+    footRotationValue = (footRotationValue + 1) % footRotationDuration;
+    var t = footRotationValue / footRotationDuration;
+    footRotation = interpolateValues(footRotationStart, footRotationEnd, armRotationTweening(t));
+}
+
+var tailRotationStart = -0.04;
+var tailRotationEnd = 0.04;
+var tailRotationDuration = 140;
+var tailRotationValue = tailRotationDuration / 2;
+var tailRotation = 0;
+
+function updateTailRotation() {
+    tailRotationValue = (tailRotationValue + 1) % tailRotationDuration;
+    var t = tailRotationValue / tailRotationDuration;
+    tailRotation = interpolateValues(tailRotationStart, tailRotationEnd, armRotationTweening(t));
 }
 
 
-function isObese() {
-    return false;
+function evaluateFood() {
+    var foodName = selectedFoodName;
+    // TODO
+}
+
+function updateMouthState() {
+    if (mouthState == 1 || mouthState == 3) {
+        mouthHalfFrameCounter += 1;
+        if (mouthHalfFrameCounter >= mouthHalfFrameAmount) {
+            mouthState = (mouthState + 1) % 4;
+        }
+    }
+
+    var isHolding = move;
+    if (!isHolding) {
+        if (mouthState == 1 || mouthState == 2) {
+            evaluateFood();
+            fatness += 1; // TODO
+        }
+
+        if (mouthState == 1) mouthState = 0;
+        if (mouthState == 2) {
+            mouthHalfFrameCounter = 0;
+            mouthState = 3;
+        }
+        return;
+    }
+
+    var x = canvas.width * 0.5;
+    var y = canvas.height * 0.25;
+    var pointerX = pointerPositionX;
+    var pointerY = pointerPositionY;
+    var dx = pointerX - x;
+    var dy = pointerY - y;
+
+    var threshold = 80;
+    var dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < threshold && mouthState == 0) {
+        mouthState = 1;
+        mouthHalfFrameCounter = 0;
+    } else if (dist >= threshold && mouthState == 2) {
+        mouthState = 3;
+        mouthHalfFrameCounter = 0;
+    } else if (dist >= threshold && mouthState == 1) {
+        mouthState = 0;
+    }
+}
+
+function getIdleHeadImageBasedOnFatness() {
+    if (fatness == 0) return images["head"];
+    else if (fatness == 1) return images["head_sad1"];
+    else if (fatness == 2) return images["head_sad1"];
+    else if (fatness == 3) return images["head_sad2"];
+    else return images["head_sad2"];
+}
+
+function getHeadImage(){
+    switch (mouthState) {
+        case 0:
+            return getIdleHeadImageBasedOnFatness();
+        case 1:
+            return images["head_half"];
+        case 2:
+            return images["head_open"];
+        default:
+            return images["head_half"];
+    }
+}
+
+function drawEllipse(centerX, centerY, width, height) {
+    context.beginPath();
+
+    context.moveTo(centerX, centerY - height / 2); // A1
+
+    context.bezierCurveTo(
+        centerX + width / 2, centerY - height / 2, // C1
+        centerX + width / 2, centerY + height / 2, // C2
+        centerX, centerY + height / 2); // A2
+
+    context.bezierCurveTo(
+        centerX - width / 2, centerY + height / 2, // C3
+        centerX - width / 2, centerY - height / 2, // C4
+        centerX, centerY - height / 2); // A1
+
+    context.fillStyle = "black";
+    context.fill();
+    context.closePath();
+}
+
+function drawShadow() {
+    var img = images["head"]; // TODO: rewrite
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.min(hRatio, vRatio);
+    var width;
+    if (fatness == 0) width = 300;
+    else if (fatness == 1) width = 330;
+    else if (fatness == 2) width = 360;
+    else if (fatness == 3) width = 390;
+    else width = 420;
+
+    drawEllipse(canvas.width / 2 - 5 * ratio, 359 * ratio, width * ratio, 50 * ratio);
+}
+
+function drawImageWithOffsetAndAngle(img, offsetX, offsetY, angle, centerX, centerY) {
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.min(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width * ratio) / 2 + offsetX * ratio;
+    var centerShift_y = (canvas.height - img.height * ratio) / 2 + offsetY * ratio;
+    var cx = centerShift_x + centerX * ratio;
+    var cy = centerShift_y + centerY * ratio;
+
+    context.translate(cx, cy);
+    context.rotate(angle);
+    context.drawImage(img, 0, 0, img.width, img.height, -centerX * ratio, -centerY * ratio, img.width * ratio, img.height * ratio);
+    context.rotate(-angle);
+    context.translate(-cx, -cy);
+}
+
+
+function drawImageWithOffset(img, offsetX, offsetY) {
+    drawImageWithOffsetAndAngle(img, offsetX, offsetY, 0, 0, 0);
+}
+
+
+function drawImageWithAngle(img, angle, centerX, centerY) {
+    drawImageWithOffsetAndAngle(img, 0, 0, angle, centerX, centerY);
+}
+
+
+function drawImageGeneral(img) {
+    drawImageWithOffset(img, 0, 0);
 }
 
 function drawHead() {
-    var x = charX - 10;
-    var y = charY - 125;
-    var img = images["head"];
+    var img = getHeadImage(images["head"]);
 
-    context.drawImage(img, x, y, img.width, img.height);
+    drawImageWithOffset(img, 0, headBobbingOffsetY);
 }
 
-function drawTorso() {
-    var x = charX;
-    var y = charY - 50;
-    var img = images["torso"];
-
-    context.drawImage(img, x, y, img.width, img.height);
+function drawBody() {
+    var img;
+    if (fatness == 0) img = images["body"];
+    else if (fatness == 1) img = images["body_fat1"];
+    else if (fatness == 2) img = images["body_fat2"];
+    else if (fatness == 3) img = images["body_fat3"];
+    else img = images["body_fat4"];
+    drawImageGeneral(img);
 }
 
-function drawEyes() {
-    var x = charX - 10;
-    var y = charY - 110;
-    var img = images["eye"];
-    context.drawImage(images["eye"], x - 5, y, img.width, img.height);
-    context.drawImage(images["eye"], x + 5, y, img.width, img.height);
+function drawHands() {
+    var img = images["left_hand"];
+    drawImageWithAngle(img, -armRotation, 216, 244);
+    img = images["right_hand"];
+    drawImageWithAngle(img, armRotation, 77, 244);
 }
 
+function drawFeet() {
+    var img = images["left_foot"];
+    drawImageWithAngle(img, footRotation, 209, 366);
+    img = images["right_foot"];
+    drawImageWithAngle(img, footRotation, 91, 366);
+}
+
+function drawTail() {
+    var img = images["tail"];
+    drawImageWithAngle(img, tailRotation, 243, 332);
+}
 
 function redraw() {
 
+    updateHeadBobbing();
+    updateArmRotation();
+    updateFootRotation();
+    updateTailRotation();
+    updateMouthState();
+
+
     clearCanvas();
 
-    drawHead();
-    drawTorso();
-    drawEyes();
-   
+    drawShadow();
+    drawTail();
+    drawBody();
+    drawHands();
+    drawFeet();
+    drawHead();   
 }
 
-
-prepareCanvas(document.getElementById("canvasDiv"), 490, 220);
+prepareCanvas(document.getElementById("canvasDiv"), $(window).width(), $(window).height() / 2);
--- www/scripts/index.js
+++ www/scripts/index.js
@@ -5,43 +5,66 @@
 move = false;
 startX = 0;
 startY = 0;
+pointerPositionX = 0;
+pointerPositionY = 0;
+selectedFoodName = "";
+
 (function () {
     "use strict";
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function movePointer(e) {
+        if (move) {
+            $("#movingImage").css({ left: e.pageX, top: e.pageY });
+            pointerPositionX = e.pageX;
+            pointerPositionY = e.pageY;
+        }
+    }
+
+    function startMovePointer(event) {
+        move = true;
+        selectedFoodName = $(this).children("img").get(0).parentElement.getAttribute("name"); // TODO: what
+        var imageUrl = $(this).children("img").get(0).getAttribute("src");
+        $("#movingImage").css({ left: event.pageX, top: event.pageY });
+        $("#movingImage").attr("src", imageUrl);
+        pointerPositionX = event.pageX;
+        pointerPositionY = event.pageY;
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    function endMovePointer(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        move = false;
+        $("#movingImage").attr("src", "");
+        $("#movingImage").css({ left: startX, top: startY });
+    }
 
     function onDeviceReady() {
         // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
         document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
 
-        $(document).mousemove(function (e) {
-            if (move) {
-                $("#movingImage").css({ left: e.pageX, top: e.pageY });
-            }
-        });
-        $(".food-button").on('mousedown', function (event) {
-            move = true;
-            var imageUrl = $(this).children("img").get(0).getAttribute("src");
-            $("#movingImage").css({ left: event.pageX, top: event.pageY });
-            $("#movingImage").attr("src", imageUrl);
-            event.preventDefault();
-            event.stopPropagation();
-        });
-        $("body").on('mouseup', function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            move = false;
-            $("#movingImage").attr("src", "");
-            $("#movingImage").css({ left: startX, top: startY });
-        });
-    };
+        var isTouchSupport = 'ontouchstart' in window;
+        var startEvent = isTouchSupport ? 'ontouchstart' : 'mousedown';
+        var moveEvent = isTouchSupport ? 'touchmove' : 'mousemove';
+        var endEvent = isTouchSupport ? 'touched' : 'mouseup';
+
+        document.addEventListener(moveEvent, movePointer, false);
+        //$(".food-button").addEventListener(startEvent, startMovePointer, false);
+        //$("body").addEventListener(endEvent, endMovePointer, false);
+        //$(document).mousemove(movePointer);
+        $(".food-button").on(startEvent, startMovePointer);
+        $("body").on(endEvent, endMovePointer);
+    }
 
     function onPause() {
         // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
-    };
+    }
 
     function onResume() {
         // TODO: Diese Anwendung wurde erneut aktiviert. Stellen Sie hier den Anwendungszustand wieder her.
-    };
+    }
 })();
